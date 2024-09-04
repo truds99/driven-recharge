96c28bf46a72736587c75f03919c59fd5c112fd5
truds99 <dudscardoso9@gmail.com>
2024-09-03 23:23:22 -0300

    add data to response phone controller

--- src/controllers/phones-controller.ts
+++ src/controllers/phones-controller.ts
@@ -1,8 +1,23 @@
 import httpStatus from "http-status";
 import { Request, Response } from "express";
-import { postPhoneService } from "../services/phones-service";
+import { getCarrierName, postPhoneService } from "../services/phones-service";
 
 export async function postPhone(req: Request, res: Response) {
     await postPhoneService(req.body);
-    res.sendStatus(httpStatus.CREATED);
+
+    const { name, customer_document, number, carrier_id, description } = req.body;
+
+    const phoneRegister = {
+        customer: {
+            name,
+            document: customer_document
+        },
+        phone: {
+            number,
+            carrier: getCarrierName(carrier_id),
+            description
+        }
+    }
+
+    res.status(httpStatus.CREATED).send(phoneRegister);
 } 
--- src/services/phones-service.ts
+++ src/services/phones-service.ts
@@ -15,3 +15,27 @@ export async function postPhoneService({name, customer_document, number, carrier
 
     await postPhoneRep(customer_document, number, carrier_id, description);
 }
+
+export function getCarrierName(carrierId: number) {
+    let carrier : string;
+
+    switch (carrierId) {
+        case 1:
+            carrier = 'Vivo';
+            break;
+        case 2:
+            carrier = 'Tim';
+            break;
+        case 3:
+            carrier = 'Oi';
+            break;
+        case 4:
+            carrier = 'Claro';
+            break;
+        default:
+            carrier = 'unknown carrier';
+            break;
+    }
+    
+    return carrier;
+}
