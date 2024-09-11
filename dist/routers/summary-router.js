378f3db6ff807a07219986c740744d0b35b4940e
truds99 <dudscardoso9@gmail.com>
2024-09-11 15:35:43 -0300

    Remove dist directory from version control

--- dist/routers/summary-router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.summaryRouter = void 0;
-var summary_controller_1 = require("../controllers/summary-controller");
-var express_1 = require("express");
-exports.summaryRouter = (0, express_1.Router)();
-exports.summaryRouter.get('/summary/:document', summary_controller_1.getSummary);
